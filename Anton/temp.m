9aa3abd517836e4112a8520346c3f85cfa51855f
christophercacciatore <Christopher@Cs-MacBook-Pro.local>
2016-01-28 17:56:29 -0600

    Framework with rails and html template

---
+++ Anton/temp.m
@@ -0,0 +1,9 @@
+//
+//  temp.m
+//  Anton
+//
+//  Created by C Cacciatore on 1/10/16.
+//  Copyright © 2016 Cacciatore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Anton_ruby/app/assets/javascripts/application.js
@@ -0,0 +1,72 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require d3
+//= require_tree .
+$.ajax({
+           type: "GET",
+           contentType: "application/json; charset=utf-8",
+           url: 'data',
+           dataType: 'json',
+           success: function (data) {
+               draw(data);
+           },
+           error: function (result) {
+               error();
+           }
+       });
+ 
+function draw(data) {
+    var color = d3.scale.category20b();
+    var width = 420,
+        barHeight = 20;
+ 
+    var x = d3.scale.linear()
+        .range([0, width])
+        .domain([0, d3.max(data)]);
+ 
+    var chart = d3.select("#graph")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
+ 
+    var bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function (d, i) {
+                  return "translate(0," + i * barHeight + ")";
+              });
+ 
+    bar.append("rect")
+        .attr("width", x)
+        .attr("height", barHeight - 1)
+        .style("fill", function (d) {
+                   return color(d)
+               })
+ 
+    bar.append("text")
+        .attr("x", function (d) {
+                  return x(d) - 10;
+              })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .style("fill", "white")
+        .text(function (d) {
+                  return d;
+              });
+}
+ 
+function error() {
+    console.log("error")
+}
